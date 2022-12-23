0731cabed164b7e3c2a69d22ceff2963d91c89ca
Eren Tutkan <eren.tutkan@indata.com.tr>
2022-12-23 09:21:32 +0300

    All Commit

--- src/components/EducationPopup.vue
+++ src/components/EducationPopup.vue
@@ -1,8 +1,14 @@
 <template>
-     <q-dialog v-model="dialog" persistent>
+     <q-dialog v-model="dialog" persistent full-width>
       <q-card>
         <q-card-section>
-          <div class="text-h6" >First dialog</div>
+          <div class="text-h6" >Eğitimler</div>
+        </q-card-section>
+        <q-card-section>
+          <div>
+            <q-table title="Eğitimi Alan Kişiler" inline :rows="rows" :columns="columns" row-key="name">
+            </q-table>
+          </div>
         </q-card-section>
         <q-card-actions>
              <q-btn flat label="Cancel" color="primary" @click="$emit('closePopup')"  />
@@ -11,12 +17,54 @@
     </q-dialog>
 </template>
 <script setup>
+const columns = [
+  {
+    name: 'name',
+    required: true,
+    label: 'Durum',
+    align: 'left',
+    field: 'Status',
+    format: val => `${val}`,
+    sortable: true
+  },
+  { name: 'tarih', align: 'center', label: 'Tarih', field: 'Date', sortable: true },
+  { name: 'egitimKodu', label: 'E. Kodu', field: 'EgitimKodu', sortable: true },
+  { name: 'egitim', label: 'Eğitim', field: 'Egitim', sortable: true },
+  { name: 'actions', label: 'İşlemler', field: '' }
+]
+const rows = [
+  {
+    Status: 'Deneme',
+    Date: 'Deneme',
+    EgitimKodu: '123',
+    Egitim: 'deneme'
+  }, {
+    Status: 'Deneme',
+    Date: 'Deneme',
+    EgitimKodu: '123',
+    Egitim: 'deneme'
+  }, {
+    Status: 'Deneme',
+    Date: 'Deneme',
+    EgitimKodu: '123',
+    Egitim: 'deneme'
+  }, {
+    Status: 'Deneme',
+    Date: 'Deneme',
+    EgitimKodu: '123',
+    Egitim: 'deneme'
+  }
+]
+
 const { defineProps } = require('@vue/reactivity')
-const { ref } = require('@vue/reactivity')
+const { ref, reactive } = require('@vue/reactivity')
 const props = defineProps([
-  'dialog'
+  'dialog',
+  'item'
 ])
 const dialog = ref(props.dialog)
 console.log(dialog)
+const item = reactive(props.item)
+console.log(item.row.Date)
 
 </script>
